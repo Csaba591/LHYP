31455b8497ac3995b7645ecf1d72da613fb65797
Csaba591 <csaba591@gmail.com>
2021-03-10 22:22:58 +0100

    update read_dataset for Patient class

--- read_dataset.py
+++ read_dataset.py
@@ -7,24 +7,26 @@ from dataset_generator import Patient
 
 if __name__ == '__main__':
     samples_path = pjoin('..', 'pickled_samples')
-    for patient_id in os.listdir(samples_path):
-        patient_dir = pjoin(samples_path, patient_id)
+    # for patient_id in os.listdir(samples_path):
+    #     patient_dir = pjoin(samples_path, patient_id)
         
-        with open(patient_dir, 'rb') as dump_file:
-            data = pickle.load(dump_file)
-        print(patient_id)
+    #     with open(patient_dir, 'rb') as dump_file:
+    #         data = pickle.load(dump_file)
+    #     print(data.id)
+    #     print(data.images[0].shape)
+    #     print(data.images[0].min(), data.images[0].max())
+    #     print(data.label)
+    #     print(data.patient_data)
+        
+    #     plt.imshow(data.images[0], cmap='gray')
+    #     plt.show()
+    patient_dir = pjoin(samples_path, '9102110AMR806.pickle')
+    with open(patient_dir, 'rb') as dump_file:
+        data = pickle.load(dump_file)
         print(data.images[0].shape)
         print(data.images[0].min(), data.images[0].max())
         print(data.label)
         print(data.patient_data)
-        
-        plt.imshow(data.images[0], cmap='gray')
+    for image in data.images:
+        plt.imshow(image, cmap='gray')
         plt.show()
-    # patient_dir = pjoin(samples_path, '9102110AMR806.pickle')
-    # with open(patient_dir, 'rb') as dump_file:
-    #     data = pickle.load(dump_file)
-    # print(data['images'][0].shape)
-    # print(data['label'])
-    # for image in data['images']:
-    #     plt.imshow(image, cmap='gray')
-    #     plt.show()
