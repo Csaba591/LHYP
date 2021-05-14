92db7017378291dbd2751249514890873a585515
Meszaros Csaba Mate <csaba591@gmail.com>
2021-05-14 21:41:48 +0200

    Remove unused and add final models

--- ml/nets.py
+++ ml/nets.py
@@ -2,140 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from ml_utils import conv_output_shape
-from torchvision.models import resnet18, resnet101
-
-
-class Linear(nn.Module):
-    def __init__(self, num_channels, input_shape, name):
-        super(Linear, self).__init__()
-        self.name = name
-
-        h, w = input_shape
-        self.fc1 = nn.Linear(in_features=num_channels*h*w, out_features=1)
-
-    def forward(self, x):
-        # start_dim = 1: don't flatten batch dimension
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = torch.sigmoid(x)
-
-        return x
-
-    @property
-    def save_path(self):
-        return self.name + '.chkpt'
-
-
-class SimpleCNN(nn.Module):
-    def __init__(self, num_channels, input_shape, name):
-        super(SimpleCNN, self).__init__()
-        self.name = name
-
-        self.conv1 = nn.Conv2d(in_channels=num_channels,
-                               out_channels=12, kernel_size=4)
-        c1h, c1w = conv_output_shape(*input_shape, 4)
-        self.fc1 = nn.Linear(in_features=12*c1h*c1w, out_features=1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        # start_dim = 1: don't flatten batch dimension
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = torch.sigmoid(x)
-
-        return x
-
-    @property
-    def save_path(self):
-        return self.name + '.chkpt'
-
-
-class BasicCNN(nn.Module):
-    def __init__(self, num_channels, input_shape, name):
-        super(BasicCNN, self).__init__()
-        self.name = name
-
-        self.conv1 = nn.Conv2d(in_channels=num_channels,
-                               out_channels=12, kernel_size=4)
-        c1_hw = conv_output_shape(*input_shape, 4)
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=4)
-        c2_hw = conv_output_shape(*c1_hw, 4)
-        self.conv3 = nn.Conv2d(in_channels=12, out_channels=6, kernel_size=3)
-
-        conv_out_h, conv_out_w = conv_output_shape(*c2_hw, 3)
-        self.fc1 = nn.Linear(in_features=6*conv_out_h *
-                             conv_out_w, out_features=1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        # start_dim = 1: don't flatten batch dimension
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = torch.sigmoid(x)
-
-        return x
-
-    @property
-    def save_path(self):
-        return self.name + '.chkpt'
-
-
-class MaxpoolCNN(nn.Module):
-    def __init__(self, num_channels, input_shape, name):
-        super(MaxpoolCNN, self).__init__()
-        self.name = name
-
-        self.conv1 = nn.Conv2d(in_channels=num_channels,
-                               out_channels=64, kernel_size=4)
-        c1_hw = conv_output_shape(*input_shape, 4)
-        self.mp1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        mp1_hw = c1_hw[0] // 2, c1_hw[1] // 2
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4)
-        c2_hw = conv_output_shape(*mp1_hw, 4)
-        mp2_hw = c2_hw[0] // 2, c2_hw[1] // 2
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3)
-
-        conv_out_h, conv_out_w = conv_output_shape(*mp2_hw, 3)
-        self.fc1 = nn.Linear(in_features=32*conv_out_h *
-                             conv_out_w, out_features=256)
-        self.fc2 = nn.Linear(in_features=256, out_features=1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.mp1(x)
-
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.mp1(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        # start_dim = 1: don't flatten batch dimension
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = F.relu(x)
-
-        x = self.fc2(x)
-        x = torch.sigmoid(x)
-
-        return x
-
-    @property
-    def save_path(self):
-        return self.name + '.chkpt'
-
+from torchvision.models import resnet18, resnet34, resnet101
 
 class DropoutCNN(nn.Module):
     def __init__(self, num_channels, input_shape, name):
@@ -262,10 +129,12 @@ class ResNet(nn.Module):
         input_shape,
         blocks,
         name,
-        activation=nn.ReLU(inplace=True)
+        activation=nn.ReLU(inplace=True),
+        use_dropout=False
     ):
         super(ResNet, self).__init__()
         self.name = name
+        self.use_dropout = use_dropout
 
         self.in_channels = 64
 
@@ -284,6 +153,9 @@ class ResNet(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(channel_sizes[-1], 1)
+
+        self.do1 = nn.Dropout(0.2)
+        self.do2 = nn.Dropout(0.1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -315,16 +187,23 @@ class ResNet(nn.Module):
         x = self.activation(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        if self.use_dropout: x = self.do1(x)
 
+        x = self.layer1(x)
+        if self.use_dropout: x = self.do1(x)
+        
+        x = self.layer2(x)
+        if self.use_dropout: x = self.do1(x)
+        
+        x = self.layer3(x)
+        if self.use_dropout: x = self.do1(x)
+        
+        x = self.layer4(x)
+        if self.use_dropout: x = self.do2(x)
+        
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
-        x = torch.sigmoid(x)
 
         return x
 
@@ -334,7 +213,7 @@ class ResNet(nn.Module):
 
 
 class LongCNN(nn.Module):
-    def __init__(self, num_channels, input_shape, name):
+    def __init__(self, num_channels, input_shape, name, conv_sizes=[64, 128, 128, 256], lin_size=512):
         super(LongCNN, self).__init__()
         self.name = name
 
@@ -343,34 +222,27 @@ class LongCNN(nn.Module):
         self.do1 = nn.Dropout(p=0.25)
         self.do2 = nn.Dropout(p=0.25)
 
-        self.conv1 = nn.Conv2d(in_channels=num_channels,
-                               out_channels=128, kernel_size=4)
-        c1_hw = conv_output_shape(*input_shape, 4)
-
+        self.conv1 = nn.Conv2d(num_channels, out_channels=conv_sizes[0], 
+            kernel_size=6, stride=3)
+        
         self.conv2 = nn.Conv2d(
-            in_channels=128, out_channels=128, kernel_size=4)
-        c2_hw = conv_output_shape(*c1_hw, 4)
-
+            in_channels=conv_sizes[0], out_channels=conv_sizes[1], 
+            kernel_size=4, stride=2)
+       
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        pool1_hw = c2_hw[0] // 2, c2_hw[1] // 2
-
+        
         self.conv3 = nn.Conv2d(
-            in_channels=128, out_channels=256, kernel_size=3)
-        c3_hw = conv_output_shape(*pool1_hw, 3)
-
+            in_channels=conv_sizes[1], out_channels=conv_sizes[2], kernel_size=3)
+        
         self.conv4 = nn.Conv2d(
-            in_channels=256, out_channels=256, kernel_size=3)
-        c4_hw = conv_output_shape(*c3_hw, 3)
-
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        pool2_hw = c4_hw[0] // 2, c4_hw[1] // 2
-
+            in_channels=conv_sizes[2], out_channels=conv_sizes[3], kernel_size=3)
+        
+        self.pool2 = nn.AdaptiveAvgPool2d((1, 1))
+        
         #self.conv5 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3)
 
-        conv_out_h, conv_out_w = pool2_hw
-        self.fc1 = nn.Linear(in_features=256*conv_out_h *
-                             conv_out_w, out_features=1024)
-        self.fc2 = nn.Linear(in_features=1024, out_features=1)
+        self.fc1 = nn.Linear(in_features=conv_sizes[-1], out_features=lin_size)
+        self.fc2 = nn.Linear(in_features=lin_size, out_features=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -379,30 +251,71 @@ class LongCNN(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
+        # x = self.pool1(x)
         x = self.relu(x)
 
         x = self.conv2(x)
         x = self.pool1(x)
         x = self.relu(x)
 
-        x = self.do1(x)
+        # x = self.do1(x)
 
         x = self.conv3(x)
+        x = self.pool1(x)
         x = self.relu(x)
 
         x = self.conv4(x)
-        x = self.pool2(x)
+        x = self.pool1(x)
         x = self.relu(x)
 
-        x = self.do2(x)
+        # x = self.do2(x)
 
-        # start_dim = 1: don't flatten batch dimension
+        x = self.pool2(x)
+
         x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
-        x = F.relu(x)
+        x = self.relu(x)
 
         x = self.fc2(x)
-        x = torch.sigmoid(x)
+
+        return x
+
+    @property
+    def save_path(self):
+        return self.name + '.chkpt'
+
+class SimmCNN(nn.Module): 
+    def __init__(self, num_channels, input_shape, name):
+        super(SimmCNN, self).__init__() 
+
+        self.name = name
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv1 = nn.Conv2d(num_channels, 32, 7, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=1)
+        
+        self.pool = nn.MaxPool2d(2, stride=2)
+        
+        self.fc1 = nn.Linear(18432, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x): 
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        
+        x = torch.flatten(x, start_dim=1) 
+    
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
 
         return x
 
@@ -415,26 +328,49 @@ def create_resnet(resnet_fn, in_channels, expansion, name):
     rn = resnet_fn()
     rn.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7,
                          stride=2, padding=3, bias=False)
+    # rn.conv1 = nn.Conv2d(
+    #         in_channels, 64, kernel_size=4, stride=2, padding=2, bias=False)
     rn.fc = nn.Linear(512 * expansion, 1)
     rn.name = name
     rn.save_path = name + '.chkpt'
     return rn
 
 
-def create_model(model_type, in_channels, input_shape, name):
+def create_model(model_type, in_channels, input_shape, name, variant):
     mt = model_type.lower()
+    var = variant.lower()
     if mt == 'resnet18':
+        if var == 'dropout':
+            return ResNet(
+                in_channels,
+                input_shape,
+                [2, 2, 2, 2],
+                name,
+                use_dropout=True
+            )
         return create_resnet(resnet18, in_channels, 1, name)
         # return nets.ResNet(in_channels, input_shape, [2,2,2,2], name)
     elif mt == 'resnet34':
-        return nets.ResNet(in_channels, input_shape, [3, 4, 6, 3], name)
+        if var == 'dropout':
+            return ResNet(
+                in_channels,
+                input_shape,
+                [3, 4, 6, 3],
+                name,
+                use_dropout=True
+            )
+        elif var == 'pytorch':
+            return create_resnet(resnet34, in_channels, 1, name)
+        return ResNet(in_channels, input_shape, [3, 4, 6, 3], name)
     elif mt == 'resnet101':
         return create_resnet(resnet101, in_channels, 4, name)
     elif mt == 'dropoutcnn':
-        return nets.DropoutCNN(in_channels, input_shape, name)
+        return DropoutCNN(in_channels, input_shape, name)
     elif mt == 'maxpoolcnn':
-        return nets.MaxpoolCNN(in_channels, input_shape, name)
+        return MaxpoolCNN(in_channels, input_shape, name)
     elif mt == 'longcnn':
         return LongCNN(in_channels, input_shape, name)
+    elif mt == 'simmcnn':
+        return SimmCNN(in_channels, input_shape, name)
     else:
         raise ValueError(f'Unknown model type \"{model_type}\"')
